b32a183ff6f1469c4210e01fe61ba0665fbf2d4f
SteecH <v.nikita2003@rambler.ru>
2025-02-15 21:08:40 +0300

    Незаконченный вариант маскировки с параметризацией. Произвольная маскировка.

--- MaskingStrings/MaskingObject.cs
+++ MaskingStrings/MaskingObject.cs
@@ -1,7 +1,12 @@
-﻿namespace MaskingStrings
+﻿using System.Text;
+
+namespace MaskingStrings
 {
     public class MaskingObject
     {
+        static readonly char[] charsMask =
+            "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ".ToCharArray();
+
         public static string ReverseMask(string inputString)
         {
             string maskString = "";
@@ -12,6 +17,39 @@
             }
 
             return maskString;
+        }
+
+        public static string RandomMask(string inputString, bool predictable)
+        {
+            Random random = new();
+            byte[] maskForString = new byte[inputString.Length];
+            byte parameter = 10;
+
+
+            for (int i = 0; i < maskForString.Length; i++)
+            {
+                if (!(inputString[i] == ' ' || inputString[i] == '\t' || inputString[i] == '\n' || inputString[i] == '\v'))
+                {
+                    if (predictable)
+                    {
+                        if (Array.IndexOf(charsMask, inputString[i]) != -1)
+                        {
+                            maskForString[i] = (byte)(33 + Array.IndexOf(charsMask, inputString[i]));
+                        }
+                        else if ((byte)(Convert.ToByte(inputString[i]) + parameter) > 127)
+                        {
+                            maskForString[i] = (byte)(33 + (byte)(Convert.ToByte(inputString[i]) + parameter) - 127);
+                        }
+                        else maskForString[i] = (byte)(Convert.ToByte(inputString[i]) + parameter);
+                    }
+                    else maskForString[i] = (byte)random.Next(33, 127);
+                }
+                else maskForString[i] = Convert.ToByte(inputString[i]);
+
+
+            }
+
+            return Encoding.ASCII.GetString(maskForString);
         }
     }
 }
--- MaskingStringsTests/MaskingStringTests.cs
+++ MaskingStringsTests/MaskingStringTests.cs
@@ -6,7 +6,15 @@ namespace MaskingStringsTests
     {
         [Theory]
         [InlineData("Hello world!!!", "!!!dlrow olleH")]
+        [InlineData("Привет мир!!!", "!!!рим тевирП")]
+        [InlineData("Иван Иванов 17.10.2000","0002.01.71 вонавИ навИ")]
         public void ReverseMaskingTest(string input, string result) => Assert.Equal(result, MaskingObject.ReverseMask(input));
+
+        [Theory]
+        [InlineData("Ivan Romanov 24345")]
+        public void RandomMaskingNonParameterTest(string input) => Assert.NotEqual(input, MaskingObject.RandomMask(input, false));
+
+
 
     }
 }
